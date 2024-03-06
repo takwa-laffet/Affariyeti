0dc9ed9d231759e2bf710f1d8fb549a978471137
MaramRezgui <101602720+MaramRezgui@users.noreply.github.com>
2024-03-06 19:38:17 +0000

    modificationInterface

--- src/main/java/tn/esprit/affariety/controllers/AffichePanier.java
+++ src/main/java/tn/esprit/affariety/controllers/AffichePanier.java
@@ -1,81 +1,79 @@
 package tn.esprit.affariety.controllers;
 
-import com.itextpdf.io.exceptions.IOException;
-import com.itextpdf.layout.element.Paragraph;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import org.w3c.dom.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import tn.esprit.affariety.models.Article;
 import tn.esprit.affariety.models.Panier;
+import tn.esprit.affariety.services.ArticleService;
 import tn.esprit.affariety.services.PanierService;
 
-import java.io.FileNotFoundException;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import tn.esprit.affariety.models.Panier;
-import tn.esprit.affariety.services.PanierService;
-
 
 public class AffichePanier {
-        @FXML
-        private VBox panierItems;
-        @FXML
-        private Label totalLabel;
-        private final PanierService panierService = new PanierService();
 
-        @FXML
-        void initialize() {
-            afficherArticlesDuPanier();
-        }
+    @FXML
+    private VBox panierItems;
+    @FXML
+    private Label totalLabel;
 
-        private void afficherArticlesDuPanier() {
-            try {
-                List<Panier> panierList = panierService.recuperer();
-                for (Panier panier : panierList) {
-                    ajouterArticleAuPanier(panier);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                // Gérer l'erreur de récupération des données du panier depuis la base de données
+    private final PanierService panierService = new PanierService();
+
+    @FXML
+    void initialize() {
+        afficherArticlesDuPanier();
+    }
+
+    private void afficherArticlesDuPanier() {
+        try {
+            List<Panier> panierList = panierService.recuperer();
+            for (Panier panier : panierList) {
+                ajouterArticleAuPanier(panier);
             }
-            float totalAchat = 0;
-            for (Node node : panierItems.getChildren()) {
-                if (node instanceof Label) {
-                    Label articleLabel = (Label) node;
-                    // Analyser le texte du Label pour extraire le prix de l'article
-                    String[] parts = articleLabel.getText().split(", Prix: ");
-                    if (parts.length == 2) { // S'assurer que le Label est au format attendu
-                        float prixArticle = Float.parseFloat(parts[1]);
-                        // Ajouter le prix de l'article au total des achats
-                        totalAchat += prixArticle;
-                    }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Gérer l'erreur de récupération des données du panier depuis la base de données
+        }
+        float totalAchat = 0;
+        for (Node node : panierItems.getChildren()) {
+            if (node instanceof Label) {
+                Label articleLabel = (Label) node;
+                // Analyser le texte du Label pour extraire le prix de l'article
+                String[] parts = articleLabel.getText().split(", Prix: ");
+                if (parts.length == 2) { // S'assurer que le Label est au format attendu
+                    float prixArticle = Float.parseFloat(parts[1]);
+                    // Ajouter le prix de l'article au total des achats
+                    totalAchat += prixArticle;
                 }
             }
-
-            // Mettre à jour l'affichage du total
-            totalLabel.setText(String.format("%.2f", totalAchat));
-
         }
 
-        private void ajouterArticleAuPanier(Panier panier) {
-            Label articleLabel = new Label();
-            articleLabel.setText("Nom: " + panier.getNom_article() + ", Prix: " + panier.getPrix()  );
-            panierItems.getChildren().add(articleLabel);
-        }
+        // Mettre à jour l'affichage du total
+        totalLabel.setText(String.format("%.2f", totalAchat));
+
+    }
+
+    private void ajouterArticleAuPanier(Panier panier) {
+        Label articleLabel = new Label();
+        articleLabel.setText("Nom: " + panier.getNom_article() + ", Prix: " + panier.getPrix());
+        panierItems.getChildren().add(articleLabel);
+    }
+
     @FXML
     void validerAchat() {
         // Initialiser le total des achats
@@ -96,50 +94,24 @@ public class AffichePanier {
         // Mettre à jour l'affichage du total
         totalLabel.setText(String.format("%.2f", totalAchat));
 
-
-
-        // Parcourir les nœuds enfants du conteneur panierItems
-        for (Node node : panierItems.getChildren()) {
-            if (node instanceof AnchorPane) {
-                AnchorPane panierItemPane = (AnchorPane) node;
-
-                // Récupérer les informations sur l'article à partir des étiquettes dans le panneau
-                Label nomLabel = (Label) panierItemPane.getChildren().get(0);
-                Label prixLabel = (Label) panierItemPane.getChildren().get(1);
-                Label quantiteLabel = (Label) panierItemPane.getChildren().get(2);
-
-                String nomArticle = nomLabel.getText();
-                float prixArticle = Float.parseFloat(prixLabel.getText());
-                int quantiteArticle = Integer.parseInt(quantiteLabel.getText());
-
-                // Calculer le total pour cet article et l'ajouter au total des achats
-                float totalArticle = prixArticle * quantiteArticle;
-                totalAchat += totalArticle;
-            }
-        }
-
-        // Mettre à jour l'affichage du total
-        totalLabel.setText(String.format("%.2f", totalAchat));
-
         // Mettre à jour les stocks dans la base de données et vider le panier
         try {
             for (Node node : panierItems.getChildren()) {
-                if (node instanceof AnchorPane) {
-                    AnchorPane panierItemPane = (AnchorPane) node;
+                if (node instanceof Label) {
+                    Label articleLabel = (Label) node;
+                    // Analyser le texte du Label pour extraire le nom de l'article
+                    String[] parts = articleLabel.getText().split(", Prix: ");
+                    if (parts.length == 2) { // S'assurer que le Label est au format attendu
+                        String nomArticle = parts[0].substring(5); // Supprimer "Nom: " du début
 
-                   //  Récupérer les informations sur l'article à partir des étiquettes dans le panneau
-                   Label nomLabel = (Label) panierItemPane.getChildren().get(0);
-                    Label prixLabel = (Label) panierItemPane.getChildren().get(1);
-                    Label quantiteLabel = (Label) panierItemPane.getChildren().get(2);
-
-                    String nomArticle = nomLabel.getText();
-                    float prixArticle = Float.parseFloat(prixLabel.getText());
-                   int quantiteArticle = Integer.parseInt(quantiteLabel.getText());
-
-                    // Mettre à jour la quantité en stock dans la base de données
-                   panierService.updateStock(nomArticle, quantiteArticle);
+                        // Mettre à jour la quantité en stock dans la base de données
+                        panierService.updateStock(nomArticle, 1); // Supposons que chaque article ait une quantité de 1 pour cet exemple
+                    }
                 }
             }
+            // Vider le panier
+            panierItems.getChildren().clear();
+
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Confirmation de commande");
             alert.setHeaderText(null);
@@ -150,85 +122,96 @@ public class AffichePanier {
             // Gérer l'erreur de mise à jour de la base de données
         }
     }
-    @FXML
-    void handlePDFGeneration(ActionEvent event) {
-        try {
-            // Créer un nouvel objet PdfWriter pour écrire dans le fichier "panier.pdf"
-            PdfWriter writer = new PdfWriter("panier.pdf");
 
-            // Créer un nouvel objet PdfDocument associé au PdfWriter
-            PdfDocument pdfDocument = new PdfDocument(writer);
-
-            // Créer un nouvel objet Document associé au PdfDocument
-            com.itextpdf.layout.Document document = new com.itextpdf.layout.Document(pdfDocument);
-
-            for (Node node : panierItems.getChildren()) {
-                if (node instanceof Label) {
-                    Label articleLabel = (Label) node;
-                    // Analyser le texte du Label pour extraire les informations sur l'article
-                    String[] parts = articleLabel.getText().split(", Prix: ");
-                    if (parts.length == 2) {
-                        String nomArticle = parts[0].substring(5);
-                        float prixArticle = Float.parseFloat(parts[1]);
-
-                        // Ajouter les informations sur l'article au document PDF
-                        document.add(new Paragraph("Nom: " + nomArticle + ", Prix: " + prixArticle));
-                    }
-                }
-            }
-
-            // Fermer le document
-            document.close();
-
-            // Afficher un message de confirmation
-            System.out.println("PDF généré avec succès !");
-        } catch (IOException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
     @FXML
     void filtrer(ActionEvent event) {
         // Créer une liste pour stocker les informations sur les articles du panier
-        List<AnchorPane> articlesPanier = new ArrayList<>();
+        List<Label> articlesPanier = new ArrayList<>();
 
-        // Ajouter tous les AnchorPane du panierItems à la liste
+        // Ajouter tous les Labels du panierItems à la liste
         for (Node node : panierItems.getChildren()) {
-            if (node instanceof AnchorPane) {
-                articlesPanier.add((AnchorPane) node);
+            if (node instanceof Label) {
+                articlesPanier.add((Label) node);
             }
         }
 
         // Trier la liste en fonction du prix de chaque article
-        Collections.sort(articlesPanier, new Comparator<AnchorPane>() {
-            @Override
-            public int compare(AnchorPane article1, AnchorPane article2) {
-                // Récupérer les prix des articles
-                float prix1 = Float.parseFloat(((Label) article1.getChildren().get(0)).getText().substring(5)); // Supprimer "Nom: " du début
-                float prix2 = Float.parseFloat(((Label) article2.getChildren().get(0)).getText().substring(5)); // Supprimer "Nom: " du début
-
-                // Comparer les prix
-                if (prix1 < prix2) {
-                    return -1;
-                } else if (prix1 > prix2) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
+        Collections.sort(articlesPanier, Comparator.comparingDouble(this::getPrixFromLabel));
 
         // Mettre à jour l'affichage du panier avec les articles triés
         panierItems.getChildren().clear();
         panierItems.getChildren().addAll(articlesPanier);
     }
+
+    private double getPrixFromLabel(Label label) {
+        String[] parts = label.getText().split(", Prix: ");
+        if (parts.length == 2) {
+            return Double.parseDouble(parts[1]);
+        }
+        return 0;
+    }
+
     @FXML
     void viderPanier() {
         panierItems.getChildren().clear();
     }
+    @FXML
+    public void pdf(ActionEvent actionEvent) throws IOException {
+        try {
+            ArticleService articleService=new ArticleService();
+            List<Article> articles = articleService.getAllArticles();
+            // Créez un nouveau document PDF
+            PDDocument document = new PDDocument();
+
+            // Créez une page dans le document
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            // Obtenez le contenu de la page
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            // Écrivez du texte dans le document
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(100, 700);
+            float totalAchat = 0;
+            for (Article article : articles) {
+                totalAchat += article.getPrix();
+            }
+
+            for (Article article : articles) {
+                String ligne = "Nom : " + article.getNomArticle() +
+                        "     Prix : " + article.getPrix();
+
+                contentStream.showText(ligne);
+                contentStream.newLine();
+                contentStream.newLineAtOffset(0, -17);
+
+
+            }
+            contentStream.showText("Total des achats : " + totalAchat);
+            contentStream.newLine();
+            contentStream.endText();
+
+            // Fermez le contenu de la page
+            contentStream.close();
+
+            // Enregistrez le document PDF
+            String outputPath = "C:/Users/user/Desktop/pidev/pidev.pdf";
+            File file = new File(outputPath);
+            document.save(file);
+
+            // Fermez le document
+            document.close();
+
+            System.out.println("PDF généré avec succès.");
+            Desktop.getDesktop().open(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void handlePDFGeneration(ActionEvent actionEvent) {
+    }
 }
-
-
-
-
-
-
